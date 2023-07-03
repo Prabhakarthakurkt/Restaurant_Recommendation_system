2621cbda97b79949ec2d2bd59e51f14bf1f1c9bc
Prabhakar Thakur <60976675+Prabhakarthakurkt@users.noreply.github.com>
2023-07-03 16:21:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+import numpy as np
+import geopandas as gpd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import folium
+
+import plotly 
+import plotly.offline as py
+import plotly.graph_objs as go
+import plotly_express as px
+
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+
+
+
